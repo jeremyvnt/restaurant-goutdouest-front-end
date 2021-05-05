dde0fbb8156b5db6b357237d7b0f8b36e44066f4
Jérémy VINCENT <jeremy.vnt@gmail.com>
2021-05-06 00:16:08 +0200

    refactor: 💡 remove useless components

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,1 @@
 export { default as Image } from "./Image";
-export { default as Icon } from "./Icon";
-export { default as LearnMoreLink } from "./LearnMoreLink";
-export { default as IconText } from "./IconText";
-export { default as ScrollTop } from "./ScrollTop";
